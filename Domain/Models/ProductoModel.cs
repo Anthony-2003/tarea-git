d2bba3d8a41dc8f951c18ef7ba09320eb2db6f3a
MasterSawXDD <62162808+MasterSawXDD@users.noreply.github.com>
2023-07-29 15:03:12 -0400

    Modelos agregados

---
+++ Domain/Models/ProductoModel.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.DAO;
+using System.Data.SqlTypes;
+using System.Data;
+
+namespace Domain.Models
+{
+    public class ProductoModel
+    {
+
+        ProductoImple productoDao = new ProductoImple();
+
+        public void insertProduct(Producto producto)
+        {
+           productoDao.insertar(producto);
+        }
+
+        public DataTable obtenerProductos()
+        {
+            return productoDao.obtenerTabla();
+        }
+
+        public byte[] obtenerFotoProducto(int id)
+        {
+            return productoDao.obtenerFotoProducto(id);
+        }
+
+        public void updateProduct(Producto producto)
+        {
+            productoDao.actualizar(producto);
+        }
+
+        public void deleteProduct(int id)
+        {
+            productoDao.borrar(id);
+        }
+
+        public DataTable buscarProducto(string descripcion)
+        {
+            return productoDao.buscar(descripcion);
+        }
+
+        public void actualizarCantidadProducto(int id, int restarCantidad)
+        {
+            productoDao.actualizarCantidadProducto(id, restarCantidad);
+        }
+ 
+    }
+}
---
+++ Domain/Models/UsuarioModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Contracts;
+using DataAccess.Entities;
+using DataAccess.Repositories;
+using DataAccess.DAO;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
+{
+    public class UsuarioModel:Usuario
+    {
+
+
+        public UsuarioModel() { }
+
+        public UsuarioModel(int idUsuario, string nombreUsuario, string contrasena)
+        {
+            this.PK_ID_USUARIO = idUsuario;
+            this.nombre_usuario = nombreUsuario;
+            this.contraseña = contrasena;
+        }
+
+        //Métodos
+
+        UsuarioImple usuarioDao = new UsuarioImple();
+
+        public bool loginUser(string user, string pass, out int idUsuario)
+        {
+            return usuarioDao.login(user, pass, out idUsuario);
+        }
+
+
+
+
+    }
+}
